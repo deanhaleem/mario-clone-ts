4ad84b48fb4a3736e8ebbe68b7c0817834a15c25
Dean Haleem <deanhaleem.10@gmail.com>
2022-04-23 12:19:26 -0400

    import assets

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,19 +6,15 @@ export default class Demo extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.atlas('scenery', 'assets/sprite-sheets/background-pipes.png', 'assets/atlas/scenery-atlas.json');
+    this.load.atlas('enemies', 'assets/sprite-sheets/enemies.png', 'assets/atlas/enemy-atlas.json');
+    this.load.atlas('items', 'assets/sprite-sheets/Items-Projectiles.png', 'assets/atlas/item-atlas.json');
+    this.load.atlas('blocks', 'assets/sprite-sheets/blocksp-pipes.png', 'assets/atlas/block-atlas.json');
+    this.load.atlas('misc', 'assets/sprite-sheets/Items-Extras.png', 'assets/atlas/misc-atlas.json');
+    this.load.atlas('mario', 'assets/sprite-sheets/mario.png', 'assets/atlas/mario-atlas.json');
   }
 
   create() {
-    const logo = this.add.image(401, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1,
-    });
+    this.add.image(50, 475, 'misc', 'hud-coin-1');
   }
 }
