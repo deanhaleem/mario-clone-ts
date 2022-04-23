04480def4af1ddc6e1e47e0b28bc0ddcfd750e7e
Dean Haleem <40346044+deanhaleem@users.noreply.github.com>
2022-04-23 13:54:46 -0400

    Audio(assets)[#7]: add audio assets and play audio on creation (#15)

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -7,9 +7,15 @@ export default class Demo extends Phaser.Scene {
 
   preload() {
     this.load.atlas('misc', 'assets/sprite-sheets/Items-Extras.png', 'assets/atlas/misc-atlas.json');
+
+    this.load.audio('theme', [
+      'assets/sound-fx/smb_1-up.wav',
+    ]);
   }
 
   create() {
     this.add.image(50, 475, 'misc', 'hud-coin-1');
+
+    this.sound.add('theme').play();
   }
 }
