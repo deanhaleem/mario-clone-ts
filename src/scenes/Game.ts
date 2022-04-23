38074ea0c4b6fb612531724c1361bdc294d60e7d
Dean Haleem <40346044+deanhaleem@users.noreply.github.com>
2022-04-23 14:20:08 -0400

    Fonts(assets)[#8]: add emulogic font and display test text (#16)

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -11,6 +11,8 @@ export default class Demo extends Phaser.Scene {
     this.load.audio('theme', [
       'assets/sound-fx/smb_1-up.wav',
     ]);
+
+    this.add.text(100, 100, 'Button', { fontFamily: 'Emulogic' });
   }
 
   create() {
