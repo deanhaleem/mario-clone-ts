233b52181b4ac7946e4281014061c3f180966616
Dean Haleem <deanhaleem.10@gmail.com>
2022-04-23 13:23:53 -0400

    remove unused code for now

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,12 +6,7 @@ export default class Demo extends Phaser.Scene {
   }
 
   preload() {
-    this.load.atlas('scenery', 'assets/sprite-sheets/background-pipes.png', 'assets/atlas/scenery-atlas.json');
-    this.load.atlas('enemies', 'assets/sprite-sheets/enemies.png', 'assets/atlas/enemy-atlas.json');
-    this.load.atlas('items', 'assets/sprite-sheets/Items-Projectiles.png', 'assets/atlas/item-atlas.json');
-    this.load.atlas('blocks', 'assets/sprite-sheets/blocksp-pipes.png', 'assets/atlas/block-atlas.json');
     this.load.atlas('misc', 'assets/sprite-sheets/Items-Extras.png', 'assets/atlas/misc-atlas.json');
-    this.load.atlas('mario', 'assets/sprite-sheets/mario.png', 'assets/atlas/mario-atlas.json');
   }
 
   create() {
