8860b61a4239a729841efcf72bf847534513ab7e
Dean Haleem <deanhaleem.10@gmail.com>
2022-04-23 22:42:59 -0400

    more setup

--- src/graphics/Sprite.ts
+++ src/graphics/Sprite.ts
@@ -2,6 +2,28 @@ export interface ISprite {
 
 }
 
-export class Sprite implements ISprite {
+type SpriteDetails = {
+  filename: string;
+  frame: { x: number, y: number, w: number, h: number };
+  spriteSourceSize: { x: number, y: number, w: number, h: number };
+  sourceSize: { w: number, h: number };
+  pivot: { x: number, y: number };
+  tint: number; // hexadecimal number
+  scale: number;
+  hitbox: { w: number, h: number };
+};
 
+export class Sprite implements ISprite {
+  private spriteDetails: SpriteDetails;
+
+  private sprite: Phaser.GameObjects.Image;
+
+  private textureName: string;
+
+  public Sprite(spriteDetails: SpriteDetails, textureName: string) {
+    this.spriteDetails = spriteDetails;
+    this.textureName = textureName;
+
+    // TODO: load sprite scale, tint etc using a singleton that maintains the current scene
+  }
 }
