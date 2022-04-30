d2d4631f6e6fd43335926fab35f26a777a638fc7
Dean Haleem <deanhaleem.10@gmail.com>
2022-04-30 19:50:17 -0400

    updating mario atlas

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -21,13 +21,15 @@ export default class Demo extends Phaser.Scene {
     // TODO: update atlas config to be like c#, except instead of the "0": [] for frames, I need to create entirely new object. key will just be dead-mario-0 for example
     // TODO: then each sprite will have image game object like below.
    // const image = this.add.image(50, 375, 'mario', 'small-left-jumping-mario');
-   const image = new Phaser.GameObjects.Image(this, 0, 0, 'mario', 'dead-mario').setScale(2)
+   const image = new Phaser.GameObjects.Image(this, 0, 0, 'mario', 'DeadMario-0-0').setScale(2).setDepth(0.99).setTint(0xA290ff)
+   const image2 = new Phaser.GameObjects.Image(this, 0, 0, 'mario', 'DeadMario-0-0').setScale(2).setDepth(0.8)
 
     //image.setScale(2).setTint(0xA290ff);
     const rt = this.add.renderTexture(0, 0, 800, 480);
     rt.beginDraw();
     //rt.batchDrawFrame('mario',  'dead-mario', 155, 200)
     rt.batchDraw(image, 510, 200)
+    rt.batchDraw(image2, 525, 200)
     rt.endDraw();
   }
 }
